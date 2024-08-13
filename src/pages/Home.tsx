80e61ed1336bd9010522b832592bb28a8d7f98e7
RaulParzianello <raulparzianello@hotmail.com>
2024-08-12 22:19:16 -0300

    uiui

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import '../styles/utility.css'
+export default function Home() {
+    return (
+        <>
+            <header>
+                <ul>
+                    <li><a>Home</a></li>
+                    <li><a>Soluções</a></li>
+                    <li><a>Depoimento</a></li>
+                    <li><a>Preços</a></li>
+                    <li><a>Contato</a></li>
+                </ul>
+            </header>
+        </>
+    )
+}
