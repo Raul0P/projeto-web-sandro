e30f5f833901a795527f731411570fbbd8ef03f8
RaulParzianello <raulparzianello@hotmail.com>
2024-11-11 15:35:27 -0300

    pricing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,8 +13,10 @@ import kkkkk from "../assets/images/icon.png";
 import "../styles/hero.css";
 import "../styles/solution.css"
 import Card from "../components/Card.tsx";
+import Check from "../assets/check.svg";
 import TestimonialCard from '../components/TestimonialCard';
 import "../styles/testimonials.css";
+import "../styles/pricing.css";
 
 
 export default function Home() {
@@ -231,6 +233,62 @@ export default function Home() {
                 </section>
             </section>
 
+
+            <section id="pricing" className="container">
+                <header>
+                    <p className="desktop-only">Planos e preços</p>
+                    <h2>Nossos planos</h2>
+                </header>
+
+                <section className="even-columns gap-1.5">
+
+                    <div className="pricing-card">
+                        <span className="plan">
+                            <h3>Básico</h3>
+                            <p>Você tem direito a uma prova das comidas DonaFrost.</p>
+                        </span><h2>Grátis</h2><Button text="Pedir agora" secondary key="free" /><span className="hr" /><span className="features">
+                            <img src={Check} alt="ícone check" width={24} height={24} />
+                            <p>Retire na loja</p>
+                        </span><span className="features">
+                            <img src={Check} alt="ícone check" width={24} height={24} />
+                            <p>Apenas 1 por CPF</p>
+                        </span>
+                    </div>
+
+                    <div className="pricing-card premium">
+                        <span className="bonus"><p>1º MÊS COM DESCONTO</p></span><span className="plan">
+                            <h3>Premium</h3>
+                            <p>Para quem precisa de uma marmita diária, muito saborosa.</p>
+                        </span><span className="price">
+                            <h2>R$ 89,90</h2>
+                            <p>/mês</p>
+                        </span><Button text="Pedir agora" key="premium" /><span className="hr" /><span className="features">
+                            <img src={Check} alt="ícone check" width={24} height={24} />
+                            <p>2 Entregas</p>
+                        </span><span className="features">
+                            <img src={Check} alt="ícone check" width={24} height={24} />
+                            <p>5 Refeições por semana</p>
+                        </span><span className="features">
+                            <img src={Check} alt="ícone check" width={24} height={24} />
+                            <p>2 Sucos por semana</p>
+                        </span>
+                    </div>
+
+                    <div className="pricing-card">
+                        <span className="plan">
+                            <h3>Básico</h3>
+                            <p>Você tem direito a uma prova das comidas DonaFrost.</p>
+                        </span><h2>Grátis</h2><Button text="Pedir agora" secondary key="free" /><span className="hr" /><span className="features">
+                            <img src={Check} alt="ícone check" width={24} height={24} />
+                            <p>Retire na loja</p>
+                        </span><span className="features">
+                            <img src={Check} alt="ícone check" width={24} height={24} />
+                            <p>Apenas 1 por CPF</p>
+                        </span>
+                    </div>
+
+                </section>
+            </section>
         </>
     )
 }
