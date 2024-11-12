da84f4df1888cb75091d3ba8f1e2ba1ff163940f
Raul0P <raulparzianello@hotmail.com>
2024-11-12 02:03:47 -0300

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://us-central1-projeto-arch-lambda-440004.cloudfunctions.net/function-send-email-auth',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
 })
